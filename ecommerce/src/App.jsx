98b588b9514337dec128acc4701fd9b77d72a6f5
xlstc110 <xlstc@gmail.com>
2026-07-23 11:38:45 -0700

    Moved Orders to React

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from 'react-router'
-import './App.css'
-import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { Routes, Route } from 'react-router';
+import './App.css';
+import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
   )
 }
