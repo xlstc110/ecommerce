e5ef52052e1fabb263b2a20cc5e73b87e32eb702
xlstc110 <xlstc@gmail.com>
2026-07-23 13:56:28 -0700

    Use axios to commit data

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -1,14 +1,13 @@
 import './HomePage.css';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
+import axios from 'axios';
 
 export function HomePage() {
-    fetch('http://localhost:3000/api/products')
+    axios.get('http://localhost:3000/api/products')
         .then((response) => {
-            return response.json()
-        }).then(data => 
-            console.log(data)
-        );
+            console.log(response.data);
+        });
     return (
         <>
             <title>Ecommerce</title>
