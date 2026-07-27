11857781e4bce1ce0bb8261a8449fe2db9af7e70
xlstc110 <xlstc@gmail.com>
2026-07-26 23:27:46 -0700

    remove redundent code

--- ecommerce/src/pages/home/HomePage.test.jsx
+++ ecommerce/src/pages/home/HomePage.test.jsx
@@ -1,6 +1,5 @@
 import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen, within } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import axios from "axios";
 import { HomePage } from "./HomePage";
 
