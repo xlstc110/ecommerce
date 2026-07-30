345988c727b26c1864a0e80f04d0133bb2fa869d
xlstc110 <xlstc@gmail.com>
2026-07-29 17:19:58 -0700

    Add openfeign for rest communication from order service to inventory service, add integration test with wiremock for this communication.

--- ecommerce-microservices/order-service/src/main/java/com/ecommerce/order_service/service/OrderService.java
+++ ecommerce-microservices/order-service/src/main/java/com/ecommerce/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ecommerce.order_service.service;
 
+import com.ecommerce.order_service.client.InventoryClient;
 import com.ecommerce.order_service.dto.OrderRequest;
 import com.ecommerce.order_service.dto.OrderResponse;
 import com.ecommerce.order_service.mapper.OrderMapper;
@@ -13,11 +14,22 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
-    public OrderResponse placeOrder(OrderRequest orderRequest) {
-        Order order = OrderMapper.orderRequestToOrder(orderRequest);
-        Order savedOrder = orderRepository.save(order);
+    public void placeOrder(OrderRequest orderRequest) {
+        boolean isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        return OrderMapper.orderToOrderResponse(savedOrder);
+        if(isInStock){
+            Order order = OrderMapper.orderRequestToOrder(orderRequest);
+            Order savedOrder = orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode "
+                    + orderRequest.skuCode()
+                    + " is insufficient");
+        }
+
+
+
+        //return OrderMapper.orderToOrderResponse(savedOrder);
     }
 }
