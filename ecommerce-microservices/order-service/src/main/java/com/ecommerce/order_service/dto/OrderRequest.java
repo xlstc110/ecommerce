b38bacc3880141c5f128cf1caf5dd115d3c6db93
xlstc110 <xlstc@gmail.com>
2026-08-01 23:00:03 -0700

    First part of Kafka

--- ecommerce-microservices/order-service/src/main/java/com/ecommerce/order_service/dto/OrderRequest.java
+++ ecommerce-microservices/order-service/src/main/java/com/ecommerce/order_service/dto/OrderRequest.java
@@ -6,5 +6,8 @@ public record OrderRequest (
         String orderNumber,
         String skuCode,
         BigDecimal price,
-        Integer quantity
-) {}
+        Integer quantity,
+        UserDetails userDetails
+) {
+    public record UserDetails(String email, String firstName, String lastName) {}
+}
--- ecommerce-microservices/order-service/src/main/java/com/ecommerce/order_service/service/OrderService.java
+++ ecommerce-microservices/order-service/src/main/java/com/ecommerce/order_service/service/OrderService.java
@@ -3,25 +3,37 @@ package com.ecommerce.order_service.service;
 import com.ecommerce.order_service.client.InventoryClient;
 import com.ecommerce.order_service.dto.OrderRequest;
 import com.ecommerce.order_service.dto.OrderResponse;
+import com.ecommerce.order_service.event.OrderPlacedEvent;
 import com.ecommerce.order_service.mapper.OrderMapper;
 import com.ecommerce.order_service.model.Order;
 import com.ecommerce.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         boolean isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
         if(isInStock){
             Order order = OrderMapper.orderRequestToOrder(orderRequest);
-            Order savedOrder = orderRepository.save(order);
+            orderRepository.save(order);
+
+            // Send the message to Kafka topic
+            // OrderNumber, email,
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic 'order-placed'", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {}", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with SkuCode "
                     + orderRequest.skuCode()
